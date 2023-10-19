6f9fb24e0f1a8977ddcbb9a3508cf026c9470212
mdmonir-hossain <monirsoftengr@gmail.com>
2023-10-19 21:37:46 +0600

    brand card load

--- src/Components/BrandCard/BrandCard.jsx
+++ src/Components/BrandCard/BrandCard.jsx
@@ -1,25 +1,20 @@
+import { Link } from "react-router-dom";
 
 
 const BrandCard = ({ brands }) => {
     const {id,name,image} = brands;
     return (
       <div>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <figure className="px-10 pt-10">
-            <img
-              src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-              className="rounded-xl"
-            />
-          </figure>
-          <div className="card-body items-center text-center">
-                    <h2 className="card-title">{ name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions">
-              <button className="btn btn-primary">Buy Now</button>
+        <Link>
+          <div className="card w-96 bg-base-100 shadow-lg bg-[#E7EBEE]">
+            <figure className="px-10 pt-10">
+              <img src={image} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title text-3xl font-extrabold">{name}</h2>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,10 +7,15 @@ const Home = () => {
     const brandData = useLoaderData();
     console.log(brandData)
     return (
-      <div className="grid grid-cols-3 container mx-auto gap-4 mt-10 mb-10">
-        {brandData.map((brands) => (
-          <BrandCard key={brands.id} brands={brands}></BrandCard>
-        ))}
+      <div className="mt-10 mb-10">
+        <h1 className="text-center text-3xl font-extrabold mb-2">
+          Our Brand Produscts
+        </h1>
+        <div className="grid grid-cols-3 container mx-auto gap-4 ">
+          {brandData.map((brands) => (
+            <BrandCard key={brands.id} brands={brands}></BrandCard>
+          ))}
+        </div>
       </div>
     );
 };
